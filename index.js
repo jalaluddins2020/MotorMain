415dcbff98c0309c136183218614e56458f0e6b0
jalaluddins2020 <jalaluddins.2020@scis.smu.edu.sg>
2021-11-02 13:09:31 +0800

    initialcommit

--- index.js
+++ index.js
@@ -51,7 +51,7 @@
         location.reload();
       }
 
-    // Update Current Mileage/Date
+    // Update eo Mileage/Date
     function updateEo() {
         var newMileage = Number(prompt("Enter new Mileage"));
         var intDate = prompt("Enter date of service (dd/mm/yyy) (if today, leave empty)");
@@ -80,3 +80,30 @@
       }
 
 
+    // Update sp Mileage/Date
+    function updateSp() {
+        var newMileage = Number(prompt("Enter new Mileage"));
+        var intDate = prompt("Enter date of service (dd/mm/yyy) (if today, leave empty)");
+        
+        while ((intDate[2] != "/" || intDate[5] != "/") && intDate != "") {
+            intDate = prompt("Enter date of service (dd/mm/yyy) (if today, leave empty)");
+        }
+
+        if (intDate == "") {
+            var today = new Date();
+            var dd = String(today.getDate()).padStart(2, '0');
+            var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+            var yyyy = today.getFullYear();
+            var newDate = dd + '/' + mm + '/' + yyyy;
+        } else {
+            var newDate = intDate;
+        }
+
+        var ref = firebase.database().ref("users/u1/bikeInfo");
+
+        ref.update ({
+        "spMile": newMileage,
+        "spDate" : newDate
+        });
+        location.reload();
+      }
