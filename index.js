7b7b5f35f5e9c46009a32353b99046ee4e0a94a5
hajparv <80242900+hajparv@users.noreply.github.com>
2021-11-09 15:41:08 +0800

    add review page slider function

--- index.js
+++ index.js
@@ -135,3 +135,10 @@ if (uid != null) {
         location.reload();
       }
 
+      /*Review Page*/
+      var slider = document.getElementById("myRange");
+      var output = document.getElementById("value");
+      output.innerHTML = slider.value;
+      slider.oninput = function() {
+      output.innerHTML = this.value;
+                      }
