a9cb1f168afd74a8d1eb269664c879bd89f43804
jalaluddins2020 <jalaluddins.2020@scis.smu.edu.sg>
2021-11-13 13:20:14 +0800

    initialcommit

--- mmscripts/shops.js
+++ mmscripts/shops.js
@@ -6,12 +6,10 @@ var shop = "shop";
 var num = 0;
 var infoMain = document.getElementById("info-main")
 var output = `<table class="w3-table">
-        <tbody>
+        <tbody class="w3-bordered">
         <tr>
             <th scope="row" class="w3-text-red"><b>Shop</b></th>
             <th>Address</th>
-            <th>Latitude</th>
-            <th>Longitude</th>
     </tr>`
 for (let obj in sessionStorage) {
     if (obj.slice(0, 4) == shop) {
@@ -23,8 +21,6 @@ for (let obj in sessionStorage) {
         output += `<tr>
         <th scope="row" class="w3-text-red"><b>`+ name +`</b></th>
         <th>`+ address +`</th>
-        <th>`+ lat +`</th>
-        <td>`+ long +`</td>
         </tr>`;
     }
 }
