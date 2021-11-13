7511bdd422101b6aa4d155c90d0eb781f3a8a0a6
Nickson Ng <nickson.ng.2020@scis.smu.edu.sg>
2021-11-13 11:24:41 +0800

    initialcommit

--- database/loginfirebase.js
+++ database/loginfirebase.js
@@ -54,7 +54,7 @@ var uiConfig = {
   },
   // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
   signInFlow: 'popup',
-  signInSuccessUrl: 'dashboard.html',
+  signInSuccessUrl: 'landing.html',
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
--- mmscripts/sessionIn.js
+++ mmscripts/sessionIn.js
@@ -2,9 +2,9 @@ var uid = sessionStorage.getItem("uid")
 
 if (uid != null) {
     if (document.location.href == "http://localhost/Motormain/signin.html") {
-        window.location.href = "dashboard.html";
+        window.location.href = "landing.html";
     } else if (document.location.href == "http://localhost/Motormain/index.html" || document.location.href == "http://localhost/Motormain/" || document.location.href == "http://localhost/Motormain")   {
-        window.location.href = "dashboard.html";
+        window.location.href = "landing.html";
     }
 }
 
